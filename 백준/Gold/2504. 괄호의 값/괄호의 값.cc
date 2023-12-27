aaf3d8bf5273712aae3f4bf11ed04603b077e103
wnsghks <122527154+wnsghks@users.noreply.github.com>
2023-12-27 18:37:07 +0900

    Update 괄호의 값.cc

--- 백준/Gold/2504. 괄호의 값/괄호의 값.cc
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.cc
@@ -1,45 +1,63 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
 using namespace std;
 
-stack<char> s;
-string str;
-
 int main(void) {
-  ios::sync_with_stdio(0);
+  ios::sync_with_stdio(false);
   cin.tie(0);
 
-  cin >> str;
-  int sum = 0; // 누적해서 더해질 값
-  int num = 1; // 곱해질 값
+  string a;
+  cin >> a;
+  
+  int temp = 1;
+  int sum = 0;
+  bool check = true; // 쌍이 안맞을 경우 종료
+  bool sign = true; // true = +. false = *
+  stack<char> s;
+  for(auto i : a) {
+    // 다른문자가 들어올경우 예외 처리
+    if(i != '(' && i != '[' && i != ')' && i != ']') {
+      check = false;
+      break;
+    }
 
-  for(int i = 0; i < str.size(); i++){
-    if(str[i] == '('){
-      num *= 2; // 소괄호가 나오면 곱해질 값 2배 증가
-      s.push(str[i]);
+    // 입력값
+    if(i == '(' || i == '[') {
+      s.push(i);
+      sign = true;
+      if(i == '(') temp *= 2;
+      else temp *= 3;
     }
-    else if(str[i] == '['){
-      num *= 3; // 대괄호가 나오면 곱해질 값 3배 증가
-      s.push(str[i]);
-    }
-    else if(str[i] == ')'){
-      if(s.empty() || s.top() != '('){
-        cout << 0;
-        return 0;
+    // ')' 경우 계산
+    else if(i == ')') {
+      if(s.empty() || s.top() == '[') {
+        check = false;
+        break;
       }
-      if(str[i-1] == '(') sum += num; // 직전 괄호가 여는 괄호였다면 sum에 값 추가
-      s.pop();
-      num /= 2; // 소괄호 쌍이 사라졌으니 2로 나눔      
-    }
-    else{ // str[i] == ']'
-      if(s.empty() || s.top() != '['){
-        cout << 0;
-        return 0;
+      else {
+        s.pop();
+        if(sign) sum += temp;
+        sign = false;
+        temp /= 2;
       }
-      if(str[i-1] == '[') sum += num; // 직전 괄호가 여는 괄호였다면 sum에 값 추가
-      s.pop();
-      num /= 3; // 대괄호 쌍이 사라졌으니 3로 나눔
+    }
+    // ']' 경우 계산
+    else if(i == ']') {
+      if(s.empty() || s.top() == '(') {
+        check = false;
+        break;
+      }
+      else {
+        s.pop();
+        if(sign) sum += temp;
+        sign = false;
+        temp /= 3;
+      }
     }
   }
-  if(s.empty()) cout << sum;
-  else cout << 0;
+  
+  if(!check || !s.empty()) sum = 0;
+  cout << sum;
+
+  return 0;
 }
