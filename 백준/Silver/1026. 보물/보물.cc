79e14b21b0d3c1d518027e3652e7c8131ee6357e
wnsghks <122527154+wnsghks@users.noreply.github.com>
2024-01-11 14:51:36 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int a[55];
+int b[55]; 
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for(int i = 0; i < n; i++) 
+		cin >> a[i];
+		for(int i = 0; i < n; i++) 
+		cin >> b[i];
+	sort(a, a+n);
+	sort(b, b+n);
+
+	int s = 0;
+	for(int i = 0; i < n; i++) {
+		s += a[i] * b[n-1-i];
+	}
+
+	cout << s << '\n';
+
+	return 0;
+}
